1382774be6ccc07f8822a21769e8a607deebc30d
itgsod-Lotfollah-zirak <lotfollah.zirak@elev.ga.it-gymnasiet.se>
2017-10-18 14:10:49 +0200

    Jag har ändrade bara lite kod

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,24 @@
-def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+def fares(prise, student=False, senior=False):
+    if student or senior:
+        print 'hel'
+        return 'halv'
+    else:
+        if prise <20 or prise >65:
+            return 'halv'
+        else:
+            return 'hel'
+
+
+
+
+# test cases
+print 'barn 10',fares(20)
+print 'pensioner 65',fares(20)
+print 'studen 25',fares(29,student=True)
+print 'vuxen 20',fares(40)
 
 
 
 
 
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+
